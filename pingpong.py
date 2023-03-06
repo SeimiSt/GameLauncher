cd65f93c4ee315317c92c581a78991043401a397
SeimiSt <125873199+SeimiSt@users.noreply.github.com>
2023-03-06 11:41:54 +0100

    Add files via upload

--- pingpong.py
+++ pingpong.py
@@ -31,32 +31,32 @@ right_pad.goto(400, 0)
  
 # Ball of circle shape
 hit_ball = turtle.Turtle()
-hit_ball.speed(40)
+hit_ball.speed(80)
 hit_ball.shape("circle")
 hit_ball.color("blue")
 hit_ball.penup()
 hit_ball.goto(0, 0)
-hit_ball.dx = 5
-hit_ball.dy = -5
+hit_ball.dx = 6
+hit_ball.dy = -6
  
  
 # Initialize the score
-left_player = 0
-right_player = 0
+player1 = 0
+player2 = 0
  
  
-# Displays the score
+# Show the score
 sketch = turtle.Turtle()
 sketch.speed(0)
 sketch.color("blue")
 sketch.penup()
 sketch.hideturtle()
 sketch.goto(0, 260)
-sketch.write("Left_player : 0    Right_player: 0",
+sketch.write("Player 1: 0    Player 2: 0",
              align="center", font=("Courier", 24, "normal"))
  
  
-# Functions to move paddle vertically
+# Functions to move paddle up and down
 def paddleaup():
     y = left_pad.ycor()
     y += 20
@@ -87,52 +87,81 @@ sc.onkeypress(paddleaup, "w")
 sc.onkeypress(paddleadown, "s")
 sc.onkeypress(paddlebup, "Up")
 sc.onkeypress(paddlebdown, "Down")
- 
- 
+
+
+
 while True:
     sc.update()
- 
-    hit_ball.setx(hit_ball.xcor()+hit_ball.dx)
-    hit_ball.sety(hit_ball.ycor()+hit_ball.dy)
- 
-    # Checking borders
-    if hit_ball.ycor() > 280:
-        hit_ball.sety(280)
-        hit_ball.dy *= -1
- 
-    if hit_ball.ycor() < -280:
-        hit_ball.sety(-280)
-        hit_ball.dy *= -1
- 
-    if hit_ball.xcor() > 500:
-        hit_ball.goto(0, 0)
-        hit_ball.dy *= -1
-        left_player += 1
-        sketch.clear()
-        sketch.write("Left_player : {}    Right_player: {}".format(
-                      left_player, right_player), align="center",
-                      font=("Courier", 24, "normal"))
- 
-    if hit_ball.xcor() < -500:
-        hit_ball.goto(0, 0)
-        hit_ball.dy *= -1
-        right_player += 1
-        sketch.clear()
-        sketch.write("Left_player : {}    Right_player: {}".format(
-                                 left_player, right_player), align="center",
-                                 font=("Courier", 24, "normal"))
- 
-    # Paddle ball collision
-    if (hit_ball.xcor() > 360 and\
-                        hit_ball.xcor() < int(dreisiebzig)) and\
-                        (hit_ball.ycor() < right_pad.ycor()+40 and\
-                        hit_ball.ycor() > right_pad.ycor()-40):
-        hit_ball.setx(360)
-        hit_ball.dx*=-1
-        
-    if (hit_ball.xcor()<-360 and\
-                       hit_ball.xcor()>-370) and\
-                       (hit_ball.ycor()<left_pad.ycor()+40 and\
-                        hit_ball.ycor()>left_pad.ycor()-40):
-        hit_ball.setx(-360)
-        hit_ball.dx*=-1
+
+    if player1 < 5 and player2 < 5:
+        hit_ball.setx(hit_ball.xcor()+hit_ball.dx)
+        hit_ball.sety(hit_ball.ycor()+hit_ball.dy)
+
+        # Checking borders
+        if hit_ball.ycor() > 280:
+            hit_ball.sety(280)
+            hit_ball.dy *= -1
+
+        if hit_ball.ycor() < -280:
+            hit_ball.sety(-280)
+            hit_ball.dy *= -1
+
+        if hit_ball.xcor() > 500:
+            hit_ball.goto(0, 0)
+            hit_ball.dy *= -1
+            player1 += 1
+            sketch.clear()
+            sketch.write("Player 1 : {}    Player 2: {}".format(
+                        player1, player2), align="center",
+                        font=("Courier", 24, "normal"))
+
+        if hit_ball.xcor() < -500:
+            hit_ball.goto(0, 0)
+            hit_ball.dy *= -1
+            player2 += 1
+            sketch.clear()
+            sketch.write("Player 1: {}    Player 2: {}".format(
+                        player1, player2), align="center",
+                        font=("Courier", 24, "normal"))
+
+        # Paddle ball collision
+        if (hit_ball.xcor() > 360 and\
+                            hit_ball.xcor() < int(dreisiebzig)) and\
+                            (hit_ball.ycor() < right_pad.ycor()+80 and\
+                            hit_ball.ycor() > right_pad.ycor()-80):
+            hit_ball.setx(360)
+            hit_ball.dx*=-1
+            
+        if (hit_ball.xcor()<-360 and\
+                        hit_ball.xcor()>-370) and\
+                        (hit_ball.ycor()<left_pad.ycor()+80 and\
+                            hit_ball.ycor()>left_pad.ycor()-80):
+            hit_ball.setx(-360)
+            hit_ball.dx*=-1
+    else:
+        if player1 > 4:
+            sketch = turtle.Turtle()
+            sketch.speed(0)
+            sketch.color("blue")
+            sketch.penup()
+            sketch.hideturtle()
+            sketch.goto(0, 100)
+            sketch.write("Player 1 Wins!",
+                        align="center", font=("Courier", 30, "normal"))
+
+        elif player2 > 4:
+            sketch = turtle.Turtle()
+            sketch.speed(0)
+            sketch.color("blue")
+            sketch.penup()
+            sketch.hideturtle()
+            sketch.goto(0, 100)
+            sketch.write("Player 2 Wins!",
+                        align="center", font=("Courier", 30, "normal"))
+
+
+
+
+
+
+
